6d4a802358debf23dd4dd75021a631b8fb0e8f60
Divangi Senjaliya <dse@narola.email>
2020-06-19 16:22:40 +0530

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,28 +3,35 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ForgetPasswordComponent } from './auth/forget-password/forget-password.component';
+import { AuthGuard } from './shared/auth.guard';
+import { LoginGuard } from './shared/login.guard';
 
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./lending-layout/lending-layout.module').then(m => m.LendingLayoutModule)
+    loadChildren: () => import('./lending-layout/lending-layout.module').then(m => m.LendingLayoutModule),
+    canActivate: [LoginGuard]
   },
   {
     path: 'sign_in',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [LoginGuard]
   },
   {
     path: 'sign_up',
-    component: RegisterComponent
+    component: RegisterComponent,
+    canActivate: [LoginGuard]
   },
   {
     path: 'forgot_password',
-    component: ForgetPasswordComponent
+    component: ForgetPasswordComponent,
+    canActivate: [LoginGuard]
   },
   {
     path: '',
-    loadChildren: () => import('./main-layout/main-layout.module').then(m => m.MainLayoutModule)
+    loadChildren: () => import('./main-layout/main-layout.module').then(m => m.MainLayoutModule),
+    canActivate: [AuthGuard]
   },
   {
     path: '**',
---
+++ src/app/shared/login.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private router: Router) { }
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const token = localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData')).TokenID._text : '';
+    if (token) {
+      // logged in so return false
+      this.router.navigate(['/sites']);
+      return false;
+    } else {
+      // not logged in so redirect to login page without the return url
+      return true;
+    }
+  }
+
+  checkLogin(url: string): boolean {
+    console.log('url  :: check here=> ', url);
+    return true;
+  }
+
+}
